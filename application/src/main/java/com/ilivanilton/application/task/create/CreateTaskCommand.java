9475a4fe56caf88cc21d0ea25a4bcffe88dfe49e
ilivanilton <ilivanilton@gmail.com>
2025-02-16 17:08:59 -0300

    refactor application layer to new attribute

--- application/src/main/java/com/ilivanilton/application/task/create/CreateTaskCommand.java
+++ application/src/main/java/com/ilivanilton/application/task/create/CreateTaskCommand.java
@@ -1,14 +1,18 @@
 package com.ilivanilton.application.task.create;
 
+import com.ilivanilton.domain.task.TaskPriority;
+
 public record CreateTaskCommand(
         String description,
+        TaskPriority priority,
         boolean isActive
 ) {
 
     public static CreateTaskCommand with(
             final String aDescription,
+            final TaskPriority aPriority,
             final boolean isActive
     ) {
-        return new CreateTaskCommand(aDescription, isActive);
+        return new CreateTaskCommand(aDescription, aPriority, isActive);
     }
 }
